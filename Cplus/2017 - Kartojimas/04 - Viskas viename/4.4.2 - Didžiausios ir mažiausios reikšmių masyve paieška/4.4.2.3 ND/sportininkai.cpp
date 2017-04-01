a4293180d0db11702a0759d4eb37a18d0cc5f90b
DreDasLT <dre2kas@gmail.com>
2017-04-01 12:33:36 +0300

    labas

--- Cplus/2017 - Kartojimas/04 - Viskas viename/4.4.2 - Didžiausios ir mažiausios reikšmių masyve paieška/4.4.2.3 ND/sportininkai.cpp
+++ Cplus/2017 - Kartojimas/04 - Viskas viename/4.4.2 - Didžiausios ir mažiausios reikšmių masyve paieška/4.4.2.3 ND/sportininkai.cpp
@@ -14,47 +14,37 @@ void Raso();
 
 int main() {
     Skaito();
-    //Raso();
+    Raso();
     return 0;
 }
 
 void Skaito() {
     fd >> a >> r >> b >> n;
-    //cout << a << " " << r << " " << b << endl << n << endl;
-    for(int i = 1; i <= n; i++) {
-        fd >> m[i] >> u[i] >> kcal[i];
-        //cout << m[i] << " " << u[i] << " " << kcal[i]  << endl;
-    }
-
+    for(int i = 1; i <= n; i++) fd >> m[i] >> u[i] >> kcal[i];
 }
-// blogai skaiciuoja funkcijos.
+
 int Didziausias(double a[]) {
     double d = 0.0; int x = 1;
-    for(int i = 1; i <= n; i++) {
-        if(a[i] > d) d = a[i], x = i;
-    }
+    for(int i = 1; i <= n; i++) if(a[i] > d) d = a[i], x = i;
     return x;
 }
 
-
 int Maziausias(double a[]) {
-    int x = n-1, maz = a[n-1];
-    for(int i = 1; i <= n; i++) {
-        if(a[i] < maz) x = i;
-    }
+    int x = n-1; double maz = a[n-1];
+    for(int i = 1; i <= n; i++) if(a[i] < maz) x = i, maz = a[i];
     return x;
 }
 
 void Raso() {
-    for(int i = 1; i <=n; i++) cout << i << " " << fixed << setprecision(0) << m[i] / (u[i]*u[i]) << endl;
+    for(int i = 1; i <= n; i++) fr << i << " " << fixed << setprecision(0) << m[i] / (u[i]*u[i]) << endl;
     int x = 0;
     x = Didziausias(u);
-    cout << x << " " << fixed << setprecision(2) << m[x] << " " << u[x] << " " <<  kcal[x] <<  setprecision(0) << " " << m[x] / (u[x]*u[x]) << endl;
+    fr << x << " " << fixed << setprecision(2) << m[x] << " " << u[x] << " " <<  kcal[x] <<  setprecision(0) << " " << m[x] / (u[x]*u[x]) << endl;
     x = Maziausias(u);
-    cout << x << " " << fixed << setprecision(2) << m[x] << " " << u[x] << " " <<  kcal[x] <<  setprecision(0) << " " << m[x] / (u[x]*u[x]) << endl;
-    x = Didziausias(m);
-    cout << x << " " << fixed << setprecision(2) << m[x] << " " << u[x] << " " <<  kcal[x] <<  setprecision(0) << " " << m[x] / (u[x]*u[x]) << endl;
+    fr << x << " " << fixed << setprecision(2) << m[x] << " " << u[x] << " " <<  kcal[x] <<  setprecision(0) << " " << m[x] / (u[x]*u[x]) << endl;
     x = Maziausias(m);
-    cout << x << " " << fixed << setprecision(2) << m[x] << " " << u[x] << " " <<  kcal[x] <<  setprecision(0) << " " << m[x] / (u[x]*u[x]) << endl;
-
+    fr << x << " " << fixed << setprecision(2) << m[x] << " " << u[x] << " " <<  kcal[x] <<  setprecision(0) << " " << m[x] / (u[x]*u[x]) << endl;
+    x = Didziausias(m);
+    fr << x << " " << fixed << setprecision(2) << m[x] << " " << u[x] << " " <<  kcal[x] <<  setprecision(0) << " " << m[x] / (u[x]*u[x]) << endl;
+    // labas 123
 }
