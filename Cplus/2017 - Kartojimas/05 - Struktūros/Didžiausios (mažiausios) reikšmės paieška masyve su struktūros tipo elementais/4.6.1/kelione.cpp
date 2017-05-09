3683cfeccb0607e6edfbf8981c0442cb562ea37d
DreDasLT <dre2kas@gmail.com>
2017-05-09 13:29:31 +0300

    Add files via upload

--- Cplus/2017 - Kartojimas/05 - Struktūros/Didžiausios (mažiausios) reikšmės paieška masyve su struktūros tipo elementais/4.6.1/kelione.cpp
+++ Cplus/2017 - Kartojimas/05 - Struktūros/Didžiausios (mažiausios) reikšmės paieška masyve su struktūros tipo elementais/4.6.1/kelione.cpp
@@ -13,9 +13,11 @@ int n;
 
 void Skaito();
 void Raso();
+int Didziausias(etapai E[], int i);
 
 int main() {
 	Skaito();
+	Raso();
 	return 0;
 }
 
@@ -23,19 +25,34 @@ void Skaito() {
 	ifstream fd("Duomenys.txt");
 	char a[20];
 	fd >> n;
-	cout << n << endl;
 	for(int i = 1; i <= n; i++) {
 		fd.ignore(80, '\n');
 		fd.get(a, 20);
 		E[i].pavadinimas = a;
 		fd >> E[i].t;
-		cout << E[i].pavadinimas << "   " << E[i].t << endl;
 		for(int z = 1; z <= E[i].t; z++) {
 			fd.ignore();
 			fd.get(a, 15);
 			E[i].nr[z] = a;
 			fd >> E[i].kv[z] >> E[i].km[z];
-			cout << E[i].nr[z] << "  " << E[i].kv[z] << " " << E[i].km[z] << endl;
 		}
 	}
+	fd.close();
+}
+
+int Didziausias(etapai E[], int i) {
+	int d = 0;
+	for(int z = 1; z <= E[i].t; z++) {
+		if(E[i].kv[z] * 60 + E[i].km[z] > E[i].kv[d] * 60 + E[i].km[d]) d = z;
+	}
+	return d;
+}
+
+void Raso() {
+	ofstream fr("Rezultatai.txt");
+	for(int i = 1; i <= n; i++) {
+		fr << E[i].pavadinimas << " " << E[i].kv[Didziausias(E, i)] << " " << E[i].km[Didziausias(E,i)] << endl;
+		fr << E[i].nr[Didziausias(E,i)] << endl;
+	}
+	fr.close();
 }
