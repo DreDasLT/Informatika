746772aa3c0282461767f403601d5dd80f30aa58
DreDasLT <dre2kas@gmail.com>
2017-05-16 14:32:17 +0300

    Add files via upload

---
+++ Cplus/2017 - Kartojimas/05 - Struktūros/Didžiausios (mažiausios) reikšmės paieška masyve su struktūros tipo elementais/4.6.3/darzoves.cpp
@@ -0,0 +1,24 @@
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+using namespace std;
+##########################################
+int ak, bk, rk, n;
+
+struct darzoves {
+	string pav;
+	double a,b,r,ev,bevk,revk,vp;
+}
+##########################################
+void Skaito();
+int Didziausias();
+int Maziausias();
+void Raso();
+
+int main() {
+	darzoves D[10];
+
+	return 0;
+}
+
+int Didziausias(darzoves D[])
