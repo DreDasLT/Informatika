2aa9a08657897bf42f633f9ff07049cf238aed76
DreDasLT <dre2kas@gmail.com>
2017-06-14 00:12:32 +0300

    Sugeneruoti jau buvusių sukurtų darbų rezultatų failai ir tęsiamas daržovių kodas.

--- Cplus/2017 - Kartojimas/05 - Struktūros/Didžiausios (mažiausios) reikšmės paieška masyve su struktūros tipo elementais/4.6.3/darzoves.cpp
+++ Cplus/2017 - Kartojimas/05 - Struktūros/Didžiausios (mažiausios) reikšmės paieška masyve su struktūros tipo elementais/4.6.3/darzoves.cpp
@@ -2,23 +2,40 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-##########################################
-int ak, bk, rk, n;
+//////////////////////////////
+double ak, bk, rk; int n;
 
 struct darzoves {
 	string pav;
-	double a,b,r,ev,bevk,revk,vp;
-}
-##########################################
+	double a,b,r,ev,bevk,revk,vkp;
+} D[10];
+//////////////////////////////
 void Skaito();
 int Didziausias();
 int Maziausias();
 void Raso();
 
 int main() {
-	darzoves D[10];
-
+	Skaito();
 	return 0;
 }
 
-int Didziausias(darzoves D[])
+void Skaito() {
+	char a[15];
+	ifstream fd("Duomenys.txt");
+	fd >> ak >> bk >> rk >> n;
+	for(int i = 1; i <= n; i++) {
+		fd.ignore();
+		fd.get(a, 15);
+		D[i].pav = a;
+		fd >> D[i].a >> D[i].b >> D[i].r;
+		//cout << D[i].pav << D[i].a << " " << D[i].b << " " << D[i].r << endl;
+		D[i].ev = D[i].a * ak + D[i].b * bk + D[i].r * rk;
+		D[i].bevk = D[i].b * bk;
+		D[i].revk = D[i].r * rk;
+		D[i].vkp = 100 - D[i].a - D[i].b - D[i].r;
+		//cout << D[i].ev << " " << D[i].bevk << " " << D[i].revk << " " << D[i].vkp << endl;
+	}
+}
+
+double Didziausias()
